f3262a59fa113ccf98af53d486dbda264038fa13
quiesscent <ciphedc@gmail.com>
2024-06-30 00:21:46 +0300

    check

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,14 @@
-import React from 'react'
+// import React from 'react'
 import {Route, createBrowserRouter, createRoutesFromElements, RouterProvider} from 'react-router-dom'
-import Header from './components/Index.jsx';
 import MainLayout from './layout/MainLayout.jsx';
-
+import Index from './components/Index.jsx';
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<MainLayout/>}>
     </Route>
 
+
 ));
 export default function App() {
     return <RouterProvider router={router}/>;
 }
-
